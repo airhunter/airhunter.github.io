c1ec8d038b14a98fd2e9ec277a4d036f919b9bdd
tompkins2000 <tompkins2000@gmail.com>
2025-12-02 17:48:36 +0800

    配置站点URL和更新项目标题及GitHub链接

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,12 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://airhunter.github.io',
+
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'Under Southern Skies',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/airhunter/' }],
 			sidebar: [
 				{
 					label: 'Guides',
